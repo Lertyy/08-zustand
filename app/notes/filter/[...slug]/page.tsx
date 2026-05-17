0db28b4e94e4cc1174b09c48803f9c5323b6d897
Lertyy <lertyy1337@gmail.com>
2026-05-17 20:12:00 +0300

    add all

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,17 +1,39 @@
+import { Metadata } from "next";
 import NotesClient from "@/components/NotesClient/NotesClient";
-import { fetchNotes } from "@/lib/api";
 
-interface PageProps {
+type Props = {
   params: {
     slug: string[];
   };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const tagFromUrl = params.slug?.[0] || "All";
+
+  const title = `Notes filtered by: ${tagFromUrl} — NoteHub`;
+  const description = `Browse your notes filtered by "${tagFromUrl}" in NoteHub. Quickly find the ideas and tasks you need.`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `https://08-zustand-five-drab.vercel.app/notes/filter/${tagFromUrl}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: `Notes filtered by ${tagFromUrl}`,
+        },
+      ],
+    },
+  };
 }
 
-export default async function Page({ params }: PageProps) {
-  const tagFromUrl = params.slug?.[0] ?? "";
-
-  // отримуємо дані тільки для стартового SSR (якщо тобі це ще потрібно)
-  const initialData = await fetchNotes("", 1, tagFromUrl);
+export default async function Page({ params }: Props) {
+  const tagFromUrl = params.slug?.[0] === "All" ? "" : params.slug?.[0] || "";
 
   return <NotesClient tag={tagFromUrl} />;
 }
